6b5b6915425ad36a181562788d2baa3dbcdb195a
Christina Kirk <christina.kirk@proton.me>
2026-03-27 10:55:51 +0100

    SearchSuggestions: titles and secondary text

--- src/components/Search/SearchSuggestions.tsx
+++ src/components/Search/SearchSuggestions.tsx
@@ -1,12 +1,15 @@
+import Box from "@mui/material/Box";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import CabinOutlined from "@mui/icons-material/CabinOutlined";
+import FilterAltOutlined from "@mui/icons-material/FilterAltOutlined";
 import PeakIcon from "./icons/PeakIcon";
 import LandscapeOutlined from "@mui/icons-material/LandscapeOutlined";
 import SearchOutlined from "@mui/icons-material/SearchOutlined";
 import { SearchWithType } from "../../features/apiSlice";
 import { HTMLAttributes } from "react";
+import { useTranslation } from "react-i18next";
 import { ReactComponent as TransportTrain } from "../../icons/svg/ic_transport_train.svg";
 
 export interface SearchSuggestionsProps {
@@ -24,13 +27,38 @@ const suggestionIconMap = {
   city: TransportTrain,
 };
 
+const suggestionTitleMap = {
+  hut: "filter.hut_filter_suggestion",
+  peak: "filter.peak_filter_suggestion",
+  range: "filter.region_filter_suggestion",
+  term: "filter.term_filter_suggestion",
+  city: "filter.city_filter_suggestion",
+};
+
 export default function SearchSuggestions({
   option,
   props,
 }: SearchSuggestionsProps) {
-  const Icon = suggestionIconMap[option.type] ?? null;
+  const { t } = useTranslation();
+  const Icon = suggestionIconMap[option.type];
+  const title = t(suggestionTitleMap[option.type]);
+  const secondaryText =
+    option.type === "range" ? (
+      <Box
+        component="span"
+        sx={{ display: "inline-flex", alignItems: "center", gap: 0.5 }}
+      >
+        <FilterAltOutlined sx={{ fontSize: "0.9rem" }} />
+        {t("filter.region_filter_helper")}
+      </Box>
+    ) : undefined;
+
   return (
-    <ListItem {...props}>
+    <ListItem
+      {...props}
+      sx={{ display: "flex", alignItems: "center", gap: 1 }}
+      title={title}
+    >
       <ListItemIcon>
         <div
           style={{
@@ -48,7 +76,11 @@ export default function SearchSuggestions({
           ) : null}
         </div>
       </ListItemIcon>
-      <ListItemText primary={option?.term} />
+      <ListItemText
+        primary={option?.term}
+        secondary={secondaryText}
+        slotProps={{ secondary: { fontSize: "0.8rem", lineHeight: 1.2 } }}
+      />
     </ListItem>
   );
 }
